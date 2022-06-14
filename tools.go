ad9cd08287c84240c7f0d89fbe7c47e453c4e997
熙凯 <xikai.wxk@antfin.com>
2022-06-14 21:57:06 +0800

    build: add github workflow for ci

---
+++ tools.go
@@ -0,0 +1,9 @@
+package tools
+
+import (
+	_ "github.com/AlekSi/gocov-xml"
+	_ "github.com/axw/gocov/gocov"
+	_ "github.com/mgechev/revive"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "gotest.tools/gotestsum"
+)
