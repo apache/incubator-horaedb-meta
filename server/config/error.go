077760be18685c041928ffc01e22210b85f0a4e6
熙凯 <xikai.wxk@antfin.com>
2022-06-20 19:52:32 +0800

    docs: add copyright for files

--- server/config/error.go
+++ server/config/error.go
@@ -1,11 +1,13 @@
+// Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
+
 package config
 
 import (
-	coderr2 "github.com/CeresDB/ceresmeta/pkg/coderr"
+	"github.com/CeresDB/ceresmeta/pkg/coderr"
 )
 
 var (
-	ErrInvalidPeerURL     = coderr2.NewCodeErrorWrapper(coderr2.InvalidParams, "invalid peers url")
-	ErrInvalidCommandArgs = coderr2.NewCodeErrorWrapper(coderr2.InvalidParams, "invalid command arguments")
-	ErrRetrieveHostname   = coderr2.NewCodeErrorWrapper(coderr2.Internal, "fail to retrieve local hostname")
+	ErrInvalidPeerURL     = coderr.NewCodeErrorWrapper(coderr.InvalidParams, "invalid peers url")
+	ErrInvalidCommandArgs = coderr.NewCodeErrorWrapper(coderr.InvalidParams, "invalid command arguments")
+	ErrRetrieveHostname   = coderr.NewCodeErrorWrapper(coderr.Internal, "fail to retrieve local hostname")
 )
--- server/error.go
+++ server/error.go
@@ -1,8 +1,8 @@
+// Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
+
 package server
 
-import (
-	coderr2 "github.com/CeresDB/ceresmeta/pkg/coderr"
-)
+import coderr2 "github.com/CeresDB/ceresmeta/pkg/coderr"
 
 var (
 	ErrCreateEtcdClient = coderr2.NewCodeErrorWrapper(coderr2.Internal, "fail to create etcd client")
