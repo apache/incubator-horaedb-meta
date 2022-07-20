95b9aa3673282245d8ab9916aaf123bc7200b873
baojinri <baojinri@163.com>
2022-07-20 18:36:25 +0800

    fix code

--- server/id/id_impl.go
+++ server/id/id_impl.go
@@ -18,7 +18,7 @@ import (
 const defaultAllocStep = uint64(1000)
 
 type AllocatorImpl struct {
-	//
+	// lock var base&end when allocator ID
 	sync.Mutex
 	base uint64
 	end  uint64
@@ -33,13 +33,17 @@ func NewAllocatorImpl(kv storage.KV, key string) *AllocatorImpl {
 func (alloc *AllocatorImpl) Alloc(ctx context.Context) (uint64, error) {
 	alloc.Lock()
 	defer alloc.Unlock()
+
 	if alloc.base == alloc.end {
 		if err := alloc.fastRebaseLocked(ctx); err != nil {
+
 			if err := alloc.rebaseLocked(ctx); err != nil {
 				return 0, err
 			}
+
 		}
 	}
+
 	alloc.base++
 	return alloc.base, nil
 }
@@ -49,55 +53,37 @@ func (alloc *AllocatorImpl) rebaseLocked(ctx context.Context) error {
 	if err != nil {
 		return errors.Wrapf(err, "get base id failed, key:%v", alloc.key)
 	}
-	end := uint64(0)
 	if value != "" {
 		value, err := strconv.ParseUint(value, 10, 64)
 		if err != nil {
 			return errors.Wrapf(err, "convert string to int failed")
 		}
-		end = uint64(value)
+		return alloc.idAllocBase(ctx, value)
 	}
-	end += defaultAllocStep
-	cmp := clientv3.Compare(clientv3.Value(func() string {
-		value, err := alloc.kv.Get(ctx, alloc.key)
-		if err != nil {
-			log.Debug("get base id failed")
-		}
-		if value == "" {
-			return "0"
-		}
-		return value
-	}()), "=", fmt.Sprintf("%020d", end-defaultAllocStep))
-	resp, err := alloc.kv.Txn(ctx).If(cmp).Then(clientv3.OpPut(alloc.key, fmt.Sprintf("%020d", end))).Commit()
-	if err != nil {
-		return errors.Wrapf(err, "put base id failed, key:%v", alloc.key)
-	} else if !resp.Succeeded {
-		return ErrTxnPutBaseId.WithCausef("txn put base id failed, resp:%v", resp)
-	}
-	log.Info("Allocator allocates a new id", zap.Uint64("alloc-id", end))
-	alloc.end = end
-	alloc.base = end - defaultAllocStep
-	return nil
+
+	return alloc.idAllocBase(ctx, 0)
 }
 
 func (alloc *AllocatorImpl) fastRebaseLocked(ctx context.Context) error {
-	end := alloc.end + defaultAllocStep
-	cmp := clientv3.Compare(clientv3.Value(func() string {
-		value, err := alloc.kv.Get(ctx, alloc.key)
-		if err != nil {
-			log.Debug("get base id failed")
-		}
-		if value == "" {
-			return "0"
-		}
-		return value
-	}()), "=", fmt.Sprintf("%020d", alloc.end))
+	return alloc.idAllocBase(ctx, alloc.end)
+}
+
+func (alloc *AllocatorImpl) idAllocBase(ctx context.Context, value uint64) error {
+	end := value + defaultAllocStep
+
+	var cmp clientv3.Cmp
+	if value == 0 {
+		cmp = clientv3.Compare(clientv3.CreateRevision(alloc.key), "=", 0)
+	} else {
+		cmp = clientv3.Compare(clientv3.Value(alloc.key), "=", fmt.Sprintf("%020d", value))
+	}
 	resp, err := alloc.kv.Txn(ctx).If(cmp).Then(clientv3.OpPut(alloc.key, fmt.Sprintf("%020d", end))).Commit()
 	if err != nil {
 		return errors.Wrapf(err, "put base id failed, key:%v", alloc.key)
 	} else if !resp.Succeeded {
 		return ErrTxnPutBaseId.WithCausef("txn put base id failed, resp:%v", resp)
 	}
+
 	log.Info("Allocator allocates a new id", zap.Uint64("alloc-id", end))
 	alloc.end = end
 	alloc.base = end - defaultAllocStep
