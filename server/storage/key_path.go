2382b2f36e2afe1f4da347a6b9f3467e3a768e86
熙凯 <xikai.wxk@antfin.com>
2022-09-09 12:09:13 +0800

    chore: fix comments

--- server/storage/key_path.go
+++ server/storage/key_path.go
@@ -19,83 +19,82 @@ const (
 	info          = "info"
 )
 
-// makeSchemaKey returns the key path to the schema meta info. For example: (cluster1)
-//
-//	v1/cluster/1/schema/info/1 -> pb.Schema
-//	v1/cluster/1/schema/info/2 -> pb.Schema
-//	v1/cluster/1/schema/info/3 -> pb.Schema
+// makeSchemaKey returns the key path to the schema meta info.
 func makeSchemaKey(clusterID uint32, schemaID uint32) string {
+	// Example:
+	//	v1/cluster/1/schema/info/1 -> pb.Schema
+	//	v1/cluster/1/schema/info/2 -> pb.Schema
+	//	v1/cluster/1/schema/info/3 -> pb.Schema
 	return path.Join(version, cluster, fmtID(uint64(clusterID)), schema, info, fmtID(uint64(schemaID)))
 }
 
-// makeClusterKey returns the cluster meta info key path. For example:
-//
-//	v1/cluster/info/1 -> pb.Cluster
-//	v1/cluster/info/2 -> pb.Cluster
-//	v1/cluster/info/3 -> pb.Cluster
+// makeClusterKey returns the cluster meta info key path.
 func makeClusterKey(clusterID uint32) string {
+	// Example:
+	//	v1/cluster/info/1 -> pb.Cluster
+	//	v1/cluster/info/2 -> pb.Cluster
+	//	v1/cluster/info/3 -> pb.Cluster
 	return path.Join(version, cluster, info, fmtID(uint64(clusterID)))
 }
 
-// makeTableKey returns the table meta info key path. For example: (cluster1, schema1)
-//
-//	v1/cluster/1/schema/1/table/1 -> pb.Table
-//	v1/cluster/1/schema/1/table/2 -> pb.Table
-//	v1/cluster/1/schema/1/table/3 -> pb.Table
+// makeTableKey returns the table meta info key path.
 func makeTableKey(clusterID uint32, schemaID uint32, tableID uint64) string {
+	// Example:
+	//	v1/cluster/1/schema/1/table/1 -> pb.Table
+	//	v1/cluster/1/schema/1/table/2 -> pb.Table
+	//	v1/cluster/1/schema/1/table/3 -> pb.Table
 	return path.Join(version, cluster, fmtID(uint64(clusterID)), schema, fmtID(uint64(schemaID)), table, fmtID(tableID))
 }
 
-// makeShardTopologyKey returns the shard meta info key path. For example: (cluster1)
-//
-//	v1/cluster/1/shard/1/1 -> pb.Shard
-//	v1/cluster/1/shard/2/1 -> pb.Shard
-//	v1/cluster/1/shard/3/1 -> pb.Shard
+// makeShardTopologyKey returns the shard meta info key path.
 func makeShardTopologyKey(clusterID uint32, shardID uint32, latestVersion string) string {
+	// Example:
+	//	v1/cluster/1/shard/1/1 -> pb.Shard
+	//	v1/cluster/1/shard/2/1 -> pb.Shard
+	//	v1/cluster/1/shard/3/1 -> pb.Shard
 	return path.Join(version, cluster, fmtID(uint64(clusterID)), shard, fmtID(uint64(shardID)), latestVersion)
 }
 
-// makeClusterTopologyKey returns the cluster topology meta info key path. For example: (cluster1)
-//
-//	v1/cluster/1/topo/1 -> pb.ClusterTopology
-//	v1/cluster/1/topo/2 -> pb.ClusterTopology
-//	v1/cluster/1/topo/3 -> pb.ClusterTopology
+// makeClusterTopologyKey returns the cluster topology meta info key path.
 func makeClusterTopologyKey(clusterID uint32, latestVersion string) string {
+	// Example:
+	//	v1/cluster/1/topo/1 -> pb.ClusterTopology
+	//	v1/cluster/1/topo/2 -> pb.ClusterTopology
+	//	v1/cluster/1/topo/3 -> pb.ClusterTopology
 	return path.Join(version, cluster, fmtID(uint64(clusterID)), topology, latestVersion)
 }
 
 // makeClusterTopologyLatestVersionKey returns the latest version info key path of cluster topology.
-// For example:
-//
-//	v1/cluster/1/topo/latestVersion -> pb.ClusterTopologyLatestVersion
-//	v1/cluster/2/topo/latestVersion -> pb.ClusterTopologyLatestVersion
-//	v1/cluster/3/topo/latestVersion -> pb.ClusterTopologyLatestVersion
 func makeClusterTopologyLatestVersionKey(clusterID uint32) string {
+	// Example:
+	//	v1/cluster/1/topo/latestVersion -> pb.ClusterTopologyLatestVersion
+	//	v1/cluster/2/topo/latestVersion -> pb.ClusterTopologyLatestVersion
+	//	v1/cluster/3/topo/latestVersion -> pb.ClusterTopologyLatestVersion
 	return path.Join(version, cluster, fmtID(uint64(clusterID)), topology, latestVersion)
 }
 
-// makeShardLatestVersionKey returns the latest version info key path of shard. For example: (cluster1)
-//
-//	v1/cluster/1/shard/1/latestVersion -> pb.ShardLatestVersion
-//	v1/cluster/1/shard/2/latestVersion -> pb.ShardLatestVersion
-//	v1/cluster/1/shard/3/latestVersion -> pb.ShardLatestVersion
+// makeShardLatestVersionKey returns the latest version info key path of shard.
 func makeShardLatestVersionKey(clusterID uint32, shardID uint32) string {
+	// Example:
+	//	v1/cluster/1/shard/1/latestVersion -> pb.ShardLatestVersion
+	//	v1/cluster/1/shard/2/latestVersion -> pb.ShardLatestVersion
+	//	v1/cluster/1/shard/3/latestVersion -> pb.ShardLatestVersion
 	return path.Join(version, cluster, fmtID(uint64(clusterID)), shard, fmtID(uint64(shardID)), latestVersion)
 }
 
-// makeNodeKey returns the node meta info key path. For example: (cluster1)
-//
-//	v1/cluster/1/node/127.0.0.1:8081 -> pb.Node
-//	v1/cluster/1/node/127.0.0.2:8081 -> pb.Node
-//	v1/cluster/1/node/127.0.0.3:8081 -> pb.Node
+// makeNodeKey returns the node meta info key path.
 func makeNodeKey(clusterID uint32, nodeName string) string {
+	// Example:
+	//	v1/cluster/1/node/127.0.0.1:8081 -> pb.Node
+	//	v1/cluster/1/node/127.0.0.2:8081 -> pb.Node
+	//	v1/cluster/1/node/127.0.0.3:8081 -> pb.Node
 	return path.Join(version, cluster, fmtID(uint64(clusterID)), node, nodeName)
 }
 
-// makeNameToIDKey return the table id key path. For example: (cluster1, schema1)
-//
-//	v1/cluster/1/schema/1/table/tableName -> tableID
+// makeNameToIDKey return the table id key path.
 func makeNameToIDKey(clusterID uint32, schemaID uint32, tableName string) string {
+	// Example:
+	//	v1/cluster/1/schema/1/table/tableName -> tableID
 	return path.Join(version, cluster, fmtID(uint64(clusterID)), schema, fmtID(uint64(schemaID)), table, tableName)
 }
 
