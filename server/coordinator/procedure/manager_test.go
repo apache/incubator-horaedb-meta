a5a9ea7e9a05105aefee2e778f5c08b014b7c451
CooooolFrog <zuliangwanghust@gmail.com>
2023-05-29 15:03:20 +0800

    refactor: refactor by cr

--- server/coordinator/procedure/manager_test.go
+++ server/coordinator/procedure/manager_test.go
@@ -92,59 +92,69 @@ func TestManager(t *testing.T) {
 	re.NoError(err)
 	re.Equal(0, len(infos))
 
-	// Test submit procedure with multi shards.
-	shardWithVersions := map[storage.ShardID]uint64{}
-	for id, view := range snapshot.Topology.ShardViewsMapping {
-		shardWithVersions[id] = view.Version
-	}
-	err = manager.Submit(ctx, &MockProcedure{
-		id:                 procedureID,
-		state:              procedure.StateInit,
-		relatedVersionInfo: procedure.RelatedVersionInfo{ClusterID: c.GetMetadata().GetClusterID(), ShardWithVersion: shardWithVersions, ClusterVersion: c.GetMetadata().GetClusterViewVersion()},
-		execTime:           time.Millisecond * 100,
-	})
-	re.NoError(err)
-	procedureID++
-	for shardID, shardView := range snapshot.Topology.ShardViewsMapping {
+	{
+		// Test schedule procedure with multi shards.
+		// 1. Submit the procedure of all shard, and all shards are locked at this time.
+		// 2. Submit procedures with single shard.
+		// 3. Because of the earliest submitted procedure, all subsequent procedures cannot be executed at this time.
+		// 4. Wait for the execution of the first procedure to complete, and the remaining procedures will be promoted and executed in parallel.
+		// 5. All procedures are executed finish.
+
+		// Test submit procedure with multi shards.
+		shardWithVersions := map[storage.ShardID]uint64{}
+		for id, view := range snapshot.Topology.ShardViewsMapping {
+			shardWithVersions[id] = view.Version
+		}
 		err = manager.Submit(ctx, &MockProcedure{
 			id:                 procedureID,
 			state:              procedure.StateInit,
-			relatedVersionInfo: procedure.RelatedVersionInfo{ClusterID: c.GetMetadata().GetClusterID(), ShardWithVersion: map[storage.ShardID]uint64{shardID: shardView.Version}, ClusterVersion: c.GetMetadata().GetClusterViewVersion()},
-			execTime:           time.Millisecond * 50,
+			relatedVersionInfo: procedure.RelatedVersionInfo{ClusterID: c.GetMetadata().GetClusterID(), ShardWithVersion: shardWithVersions, ClusterVersion: c.GetMetadata().GetClusterViewVersion()},
+			execTime:           time.Millisecond * 100,
 		})
+		re.NoError(err)
 		procedureID++
+		for shardID, shardView := range snapshot.Topology.ShardViewsMapping {
+			err = manager.Submit(ctx, &MockProcedure{
+				id:                 procedureID,
+				state:              procedure.StateInit,
+				relatedVersionInfo: procedure.RelatedVersionInfo{ClusterID: c.GetMetadata().GetClusterID(), ShardWithVersion: map[storage.ShardID]uint64{shardID: shardView.Version}, ClusterVersion: c.GetMetadata().GetClusterViewVersion()},
+				execTime:           time.Millisecond * 50,
+			})
+			procedureID++
+			re.NoError(err)
+		}
+
+		// Procedure with multi shard is running, all shard is locked.
+		time.Sleep(time.Millisecond * 10)
+		infos, err = manager.ListRunningProcedure(ctx)
+		re.NoError(err)
+		re.Equal(len(snapshot.Topology.ShardViewsMapping), len(infos))
+
+		// Procedure with multi shard is still running, all shard is still locked.
+		time.Sleep(time.Millisecond * 50)
+		infos, err = manager.ListRunningProcedure(ctx)
+		re.NoError(err)
+		re.Equal(len(snapshot.Topology.ShardViewsMapping), len(infos))
+
+		// Procedure with multi shard is finished.
+		time.Sleep(time.Millisecond * 50)
+		infos, err = manager.ListRunningProcedure(ctx)
+		re.NoError(err)
+		re.Equal(0, len(infos))
+
+		// Waiting for next promote, procedures with single shard will be scheduled.
+		time.Sleep(time.Millisecond * 500)
+		infos, err = manager.ListRunningProcedure(ctx)
+		re.NoError(err)
+		re.Equal(len(snapshot.Topology.ShardViewsMapping), len(infos))
+
+		// procedures with single shard is finished.
+		time.Sleep(time.Millisecond * 50)
+		infos, err = manager.ListRunningProcedure(ctx)
+		re.NoError(err)
+		re.Equal(0, len(infos))
+
+		err = manager.Stop(ctx)
 		re.NoError(err)
 	}
-	// Procedure with multi shard is running, all shard is locked.
-	time.Sleep(time.Millisecond * 10)
-	infos, err = manager.ListRunningProcedure(ctx)
-	re.NoError(err)
-	re.Equal(len(snapshot.Topology.ShardViewsMapping), len(infos))
-
-	// Procedure with multi shard is still running, all shard is still locked.
-	time.Sleep(time.Millisecond * 50)
-	infos, err = manager.ListRunningProcedure(ctx)
-	re.NoError(err)
-	re.Equal(len(snapshot.Topology.ShardViewsMapping), len(infos))
-
-	// Procedure with multi shard is finished.
-	time.Sleep(time.Millisecond * 50)
-	infos, err = manager.ListRunningProcedure(ctx)
-	re.NoError(err)
-	re.Equal(0, len(infos))
-
-	// Waiting for next promote, procedures with single shard will be scheduled.
-	time.Sleep(time.Millisecond * 500)
-	infos, err = manager.ListRunningProcedure(ctx)
-	re.NoError(err)
-	re.Equal(len(snapshot.Topology.ShardViewsMapping), len(infos))
-
-	// procedures with single shard is finished.
-	time.Sleep(time.Millisecond * 50)
-	infos, err = manager.ListRunningProcedure(ctx)
-	re.NoError(err)
-	re.Equal(0, len(infos))
-
-	err = manager.Stop(ctx)
-	re.NoError(err)
 }
