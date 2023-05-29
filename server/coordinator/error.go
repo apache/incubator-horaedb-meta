6952d2323523d8fad1ffadcdf0c4a7c3263610ba
baojinri <baojinri@gmail.com>
2023-05-29 13:28:51 +0800

    add cluster state judge in table creation

--- server/coordinator/error.go
+++ server/coordinator/error.go
@@ -7,4 +7,5 @@ import "github.com/CeresDB/ceresmeta/pkg/coderr"
 var (
 	ErrNodeNumberNotEnough = coderr.NewCodeError(coderr.Internal, "node number not enough")
 	ErrPickNode            = coderr.NewCodeError(coderr.Internal, "no node is picked")
+	ErrClusterNotStable    = coderr.NewCodeError(coderr.Internal, "cluster state not stable")
 )
