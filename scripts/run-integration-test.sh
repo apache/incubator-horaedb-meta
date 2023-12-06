ae725455d556d5e33b0c38657abdb51fd9928e89
CooooolFrog <zuliangwanghust@gmail.com>
2023-12-06 15:06:01 +0800

    fix integration test

--- scripts/run-integration-test.sh
+++ scripts/run-integration-test.sh
@@ -16,14 +16,14 @@
 
 set -exo
 
-CERESMETA_BIN_PATH="$(pwd)/bin/ceresmeta-server"
+META_BIN_PATH="$(pwd)/bin/ceresmeta-server"
 INTEGRATION_TEST_PATH=$(mktemp -d)
 
 # Download CeresDB Code
 cd $INTEGRATION_TEST_PATH
-git clone --depth 1 https://github.com/ceresdb/ceresdb.git --branch main
+git clone --depth 1 https://github.com/ceresdb/ceresdb.git --branch dev
 
 # Run integration_test
 cd ceresdb/integration_tests
 
-CERESMETA_BIN_PATH=$CERESMETA_BIN_PATH make run-cluster
+META_BIN_PATH=$META_BIN_PATH make run-cluster
