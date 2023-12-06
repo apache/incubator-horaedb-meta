cec9fe04b8e361f141840e9b79041ec5c714c8a8
CooooolFrog <zuliangwanghust@gmail.com>
2023-12-06 16:29:10 +0800

    fix: fix integration tests on dev branch (#287)
    ## Rationale
    Currently the tests on the dev branch are not running properly.
    
    ## Detailed Changes
    * Cherry pick some changes on main branch.
    * Fix integration test.
    
    ## Test Plan
    Pass CI.

--- server/coordinator/procedure/storage_impl.go
+++ server/coordinator/procedure/storage_impl.go
@@ -1,20 +1,17 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright 2022 The CeresDB Authors
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package procedure
@@ -27,8 +24,8 @@ import (
 	"path"
 	"strconv"
 
-	"github.com/apache/incubator-horaedb-meta/pkg/log"
-	"github.com/apache/incubator-horaedb-meta/server/etcdutil"
+	"github.com/CeresDB/ceresmeta/pkg/log"
+	"github.com/CeresDB/ceresmeta/server/etcdutil"
 	"github.com/pkg/errors"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.etcd.io/etcd/client/v3/clientv3util"
