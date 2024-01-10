71792f28ab30f77cda7cf7cba7720a2575f6478c
CooooolFrog <zuliangwanghust@gmail.com>
2024-01-10 16:43:47 +0800

    rebase on main

--- server/etcdutil/util.go
+++ server/etcdutil/util.go
@@ -1,9 +1,11 @@
 /*
- * Copyright 2022 The CeresDB Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -21,7 +23,7 @@ import (
 	"context"
 	"path"
 
-	"github.com/CeresDB/ceresmeta/pkg/log"
+	"github.com/apache/incubator-horaedb-meta/pkg/log"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.uber.org/zap"
 )
